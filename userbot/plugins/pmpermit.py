735496981384bad369385d2aef0c8ed1c6bfa972
VWO53 <64356181+VWO53@users.noreply.github.com>
2020-07-17 10:39:50 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -21,10 +21,10 @@ PREV_REPLY_MESSAGE = {}
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**Set ALIVE_NAME in config vars in Heroku**"
 USER_BOT_WARN_ZERO = "`You were spamming my sweet master's inbox, henceforth your retarded lame ass has been blocked by my master's userbot⭕️.`\n**Now GTFO, i'm playing minecraft**"
 
-USER_BOT_NO_WARN = ("`Hello, This is AntiSpam Security Service⚠️.You have found your way here to my master,`"
+USER_BOT_NO_WARN = ("`Hello, This is AntiSpam Security Service.You have found your way here to my master,`"
                     f"{DEFAULTUSER}'s `inbox.\n"
                     "Leave your Name,Reason and 10k$ and hopefully you'll get a reply within 2 light years.`\n\n"
-                    " Register Your Request! \nSend /start To Register Your Request!! \n"
+                    " This is Illegal And Regarded As A Crime \n"
                     "**Now You Are In Trouble So Send** `/start`  **To Start A Valid Conversation!!**")
 
 
